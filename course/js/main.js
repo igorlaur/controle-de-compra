ced889f85030e9bdbedbe57a09cd3bb7b9cabc7e
laur <igor.laur2@gmail.com>
2018-12-07 09:45:39 -0200

    Adicionado função Desc, Value e addData

--- course/js/main.js
+++ course/js/main.js
@@ -18,10 +18,32 @@ function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     // Percorrer a lista
     for(var key in list){
-        table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td>Edit | Delete</td></tr>';
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td>Edit | Delete</td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
+}
+
+function formatDesc(desc){
+    var str = desc.toLowerCase(); // Tratando texto para minúsculo
+    str = str.charArt(0).toUpperCase() + str.slice(1); // Primeiro caracter da string // Deixa-lo maiúsculo // Concatenar com restante
+    return str;
+}
+
+function formatValue(value){
+    var str = parseFloat(value).toFixed(2) + ""; // Transformando em float // 2 casas
+    str = str.replace(".", ","); // Transformando o . em ,
+    str = "$ " + str; 
+    return str;
+}
+
+function addData(){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list.unshift({"desc":desc , "amount":amount , "value":value});
+    setList(list); // clica botão e acrescenta novos dados na tabela no html
 }
 
 setList(list);
